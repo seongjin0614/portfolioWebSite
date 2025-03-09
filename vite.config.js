4da519257979aeb4377819fdaf887ca85ac1729b
서성진 <tjtjdwls0614@gmail.com>
2025-03-09 15:57:27 +0900

    포트폴리오 웹사이트 재시작

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('lottie-react')) {
-              return 'lottie';
-            }
-            if (id.includes('@fullpage/react-fullpage')) {
-              return 'fullpage';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['lottie-react', '@fullpage/react-fullpage']
-  },
-  resolve: {
-    dedupe: ['react', 'react-dom']
-  }
-});
+})
