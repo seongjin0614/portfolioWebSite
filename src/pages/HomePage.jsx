19795215cc1b1d9157a79aa94fb55dbbca3a28c8
서성진 <tjtjdwls0614@gmail.com>
2025-03-09 17:05:39 +0900

    typed 라이브러리 적용

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,7 @@ import {useEffect} from "react";
 
 export default function HomePage() {
 
+  // 풀페이지
   useEffect(() => {
     new fullpage("#fullpage", {
       autoScrolling: true,
@@ -16,15 +17,14 @@ export default function HomePage() {
   }, []);
 
   return (
+
     // fullpage를 적용하려는 태그의 id를 "fullpage"로 할당 해야함
     // section은 className이 "section"로 할당 해야함
-    
     <div id="fullpage">
       <IntroSection className="section"/>
       <div className="section">섹션 1</div>
       <div className="section">섹션 2</div>
       <div>섹션 3</div>
     </div>
-
   )
 };
--- src/sections/IntroSection.jsx
+++ src/sections/IntroSection.jsx
@@ -1,7 +1,45 @@
-
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
 
 export default function IntroSection({className}) {
+
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(textRef.current, {
+      strings: [
+        "✅ 제가 생각하는 ‘좋은 개발자’란 함께 일하고 싶은 동료입니다. <br> ✅ 코드 리뷰와 기술 토론을 통해 함께 성장하는 문화를 중요하게 생각합니다. <br> ✅ 단순한 개발을 넘어 비즈니스적 관점에서 아이디어를 제시하는 '서비스 중심 개발자'를 목표로 합니다."
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      startDelay: 500,
+      backDelay: 1000,
+      loop: true,
+      showCursor: true,
+      cursorChar: '|'
+    });
+
+    return ()=> {
+      typed.destroy();
+    };
+  }, []);
+
+
   return (
-    <div className={className}>인트로 섹션</div>
+    <div className={className}>
+      
+      <div>
+        <div>
+          <span>Function</span><span> FrontEnd_Developer</span><span>()</span><span></span>
+        </div>
+        <div>
+          <span ref={textRef}></span>
+        </div>
+      </div>
+
+      <div>
+      </div>
+
+    </div>
   )
 }
